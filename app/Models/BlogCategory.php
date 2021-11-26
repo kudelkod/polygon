4bbe20b0fc48cb2a53b2588632a630cd025a683c
Dmitriy <kudelkod63@gmail.com>
2021-11-26 22:11:31 +0300

    Acsessor

--- app/Models/BlogCategory.php
+++ app/Models/BlogCategory.php
@@ -9,14 +9,46 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 /**
  * @property mixed $title
  * @property mixed $slug
+ * @property mixed $id
  */
 class BlogCategory extends Model
 {
     use SoftDeletes;
     use HasFactory;
 
+    const ROOT = 1;
+
     protected $fillable = ['title',
                            'slug',
                            'parent_id',
                            'description',];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parentCategory(){
+        return $this->belongsTo(BlogCategory::class, 'parent_id', 'id');
+    }
+
+    /**
+     * @return string
+     */
+    public function getParentTitleAttribute(){
+        $title = $this->parentCategory->title
+            ?? ($this->isRoot()
+            ? 'Корень'
+            : '???');
+
+        return $title;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isRoot()
+    {
+        return $this->id === BlogCategory::ROOT;
+    }
 }
+
+
--- app/Repositories/BlogCategoryRepository.php
+++ app/Repositories/BlogCategoryRepository.php
@@ -53,6 +53,9 @@ class BlogCategoryRepository extends CoreRepository
         $result = $this
             ->startConditions()
             ->select($columns)
+            ->with([
+                'parentCategory:id,title',
+                ])
             ->paginate($perPage);
 
         return $result;
